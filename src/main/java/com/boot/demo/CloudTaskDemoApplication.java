92b66e0abd9a56ecbae11d91a30f1de3cecb8d44
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-24 21:39:58 +0530

    Spring Boot Cloud TASK Project

---
+++ src/main/java/com/boot/demo/CloudTaskDemoApplication.java
@@ -0,0 +1,13 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTaskDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTaskDemoApplication.class, args);
+	}
+
+}
