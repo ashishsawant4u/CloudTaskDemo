b6987d9100b4baa3946edf8dd883eb74a7e99875
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-25 17:57:58 +0530

    Life cycle events of Task

--- src/main/java/com/boot/demo/DemoTask1.java
+++ src/main/java/com/boot/demo/DemoTask1.java
@@ -1,6 +1,10 @@
 package com.boot.demo;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.task.listener.annotation.AfterTask;
+import org.springframework.cloud.task.listener.annotation.BeforeTask;
+import org.springframework.cloud.task.listener.annotation.FailedTask;
+import org.springframework.cloud.task.repository.TaskExecution;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,5 +16,23 @@ public class DemoTask1 implements CommandLineRunner{
 		System.out.println("WLCOME TO CLOUD TASK "+args[1]);
 		
 	}
+	
+	@BeforeTask
+	public void taskStartHandler(TaskExecution execution)
+	{
+		System.out.println("Task Started for execution id --> "+execution.getExecutionId());
+	}
+	
+	@AfterTask
+	public void taskEndHandler(TaskExecution execution)
+	{
+		System.out.println("Task Ended for execution id -->  "+execution.getExecutionId());
+	}
 
+	@FailedTask
+	public void taskFailureHandler(TaskExecution execution)
+	{
+		System.out.println("Task Failed for execution id -->  "+execution.getExecutionId());
+	}
+	
 }
